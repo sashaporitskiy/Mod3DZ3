e0cc4a6c6fd09850d2109afe06506a3a934c2c0e
sashaporitskiy <31014377+sashaporitskiy@users.noreply.github.com>
2017-09-19 21:52:22 +0300

    q

--- src/DopZad3.java
+++ src/DopZad3.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class DopZad3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("введите размер массива");
+        String line = in.nextLine();
+        String[] numbers = line.split( " ");
+        int[] lis = new int[numbers.length];
+        for(int i=0;i<numbers.length;i++) {
+            lis[i] = Integer.parseInt(numbers[i]);
+        }
+        System.out.print("введенный масив { ");
+        for (int i :lis) {
+            System.out.print(i+", ");
+        }
+        System.out.println(" } ");
+    }
 }
